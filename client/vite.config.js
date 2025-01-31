dbf6cba9f261e0a4b1ccc61bda437d98c1386128
Xiangyu Guan <xiangyuguan@Xiangyus-MacBook-Pro-2.local>
2025-01-31 02:23:10 -0500

    gooddd

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  root: path.resolve(__dirname, "client"), // Set the root directory for Vite
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    outDir: path.resolve(__dirname, "client/dist"), // Output directory for production build
   },
   server: {
-    host: true,
-    strictPort: true,
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/socket.io": {
+        target: "http://localhost:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
 });
